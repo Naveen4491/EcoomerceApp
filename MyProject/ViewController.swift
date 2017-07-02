a93f0e11cff8bc3a25ea5b17e054327bf30db695
Naveen <naveen@Naveens-MacBook-Air.local>
2017-07-02 17:08:56 +0530

    i have added map view

--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("naveen")
+        
+        let mapview = mapviewcontroller()
+        print(mapview)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
